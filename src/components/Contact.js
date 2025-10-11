acb9b878b4d758eb915836e20e7ec542af504bd6
Arwynor <142296765+Arwynor@users.noreply.github.com>
2025-10-12 00:50:08 +0100

    fixed typo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Contact() {
     return (
         <div className='sectionPage'>
-            <p>For all enquireies contact us at info@wtsdu.com</p>
+            <p>For all enquiries contact us at info@wtsdu.com</p>
         </div>
     );
 }
